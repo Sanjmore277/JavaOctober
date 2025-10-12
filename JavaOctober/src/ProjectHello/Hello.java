4a015c913ab466ffb9461d2626f581478a854835
sanjmore <sanjmore@INLEN8520066250.corp.capgemini.com>
2025-10-12 16:25:52 +0530

    Commit change

--- JavaOctober/src/ProjectHello/Hello.java
+++ JavaOctober/src/ProjectHello/Hello.java
@@ -4,8 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-            System.out.println("Hello Sanjay More Capgemini");
-            System.out.println("2+2");
+          System.out.println("Hello world"); 
 	}
 
 }
